6e8666916a61f1e694b2b937bc3abbf71659e9d4
aburaihan8989 <yusup.smartphone@gmail.com>
2025-08-09 07:26:38 +0700

    update

--- resources/views/pages/orders/index.blade.php
+++ resources/views/pages/orders/index.blade.php
@@ -102,12 +102,31 @@
                             </div>
                             <div class="card-body">
 
-                                <div class="float-right">
+                                <div class="float-left">
                                     <form method="GET" action="{{ route('order.index') }}">
                                         <div class="input-group">
-                                            <input type="date" class="form-control" placeholder="Search Date" name="transaction_time">
-                                            <div class="input-group-append">
-                                                <button class="btn btn-primary"><i class="fas fa-search"></i></button>
+                                            <div class="form-group">
+                                                <label>Pilih By Bulan : </label>
+                                                <div class="input-group">
+                                                    {{-- <input type="date" class="form-control" name="transaction_time" value="<?php echo date('Y-m-d');?>"> --}}
+                                                    <input type="month" class="form-control" style="width: 200px;" name="transaction_time">
+                                                    <button class="btn btn-primary mr-4"><i class="fas fa-search"></i></button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+
+                                <div class="float-left">
+                                    <form method="GET" action="{{ route('order.index') }}">
+                                        <div class="input-group">
+                                            <div class="form-group">
+                                                <label>Pilih By Tanggal : </label>
+                                                <div class="input-group">
+                                                    {{-- <input type="date" class="form-control" name="transaction_time" value="<?php echo date('Y-m-d');?>"> --}}
+                                                    <input type="date" class="form-control" style="width: 200px;" name="transaction_time">
+                                                    <button class="btn btn-primary"><i class="fas fa-search"></i></button>
+                                                </div>
                                             </div>
                                         </div>
                                     </form>
